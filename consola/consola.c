dca0b663cda8a4b74a7be54ebd806116fd621eac
tomasbernat <83042701+tomasbernat@users.noreply.github.com>
2022-05-14 21:59:35 -0300

    Add files via upload

--- consola/consola.c
+++ consola/consola.c
@@ -8,10 +8,16 @@ int main(int argc, char** argv){
 		return EXIT_FAILURE;
 	}
 
+	// PARSEAR INSTRUCCIONES
+	t_config* config;
+	int socket_cliente;
+	uint32_t handshake = 8080;
+	char* ip = string_new();
+	char* puerto = string_new();
 	char c = '\0';
 	char caracter[1];
 	char* buffer = string_new();
-	char** listaInstrucciones = string_array_new();
+	uint32_t exit_code = 1;
 	FILE* archivo = fopen(argv[1], "r");
 
 
@@ -20,23 +26,107 @@ int main(int argc, char** argv){
 		string_append(&buffer, caracter);
 	}
 
-	listaInstrucciones = string_split(buffer, "\n");
+	int size = strlen(buffer);
 
-	int size = string_array_size(listaInstrucciones);
-	printf("%d \n", size);
-	for(int i = 0; i < size; i++) {
-		printf("%s \n", listaInstrucciones[i]);
-	}
+	// SOCKETS
+	// ASEGURARSE DE QUE EL PROCESO KERNEL ESTE ABIERTO
 
+	config = inicializarConfigs();
+
+	ip = config_get_string_value(config, "SERVER");
+	puerto = config_get_string_value(config, "PUERTO");
+
+	printf("ip: %s \n", ip);
+	printf("puerto: %s \n", puerto);
+
+	socket_cliente = crear_conexion(ip, puerto); // SE CONECTA AL KERNEL
+
+	t_info_proceso infoProceso;
+
+	infoProceso.tamanioDirecciones = (uint32_t)atoi(argv[2]);
+	printf("TAMANIO: %d \n", infoProceso.tamanioDirecciones);
+	infoProceso.largoListaInstrucciones = (uint32_t)(size+1);
+	printf("LARGO LISTA: %d \n", infoProceso.largoListaInstrucciones);
+	infoProceso.listaInstrucciones = malloc(size+1);
+	strcpy(infoProceso.listaInstrucciones, buffer);
+	printf("LISTA DE INSTRUCCIONES:\n%s \nFIN LISTA \n", infoProceso.listaInstrucciones);
+	//t_info_proceso listo
+
+	t_buffer* bufferinho = malloc(sizeof(t_buffer));
+	size = sizeof(uint32_t) * 2 + infoProceso.largoListaInstrucciones;
+	bufferinho -> size = size;
+	void* stream = malloc(size);
+	int offset = 0;
+
+	memcpy(stream+offset, &(infoProceso.tamanioDirecciones), sizeof(uint32_t));
+	offset = offset + sizeof(uint32_t);
+	memcpy(stream+offset, &(infoProceso.largoListaInstrucciones), sizeof(uint32_t));
+	offset = offset + sizeof(uint32_t);
+	memcpy(stream+offset, infoProceso.listaInstrucciones, infoProceso.largoListaInstrucciones);
+
+	bufferinho->stream = stream;
+	//t_buffer listo
+
+	t_paquete* paquete = malloc( sizeof(t_paquete) );
+
+	paquete->codOp = CREAR_PROCESO;
+	paquete->buffer = bufferinho;
+
+	void* a_enviar = malloc( sizeof(uint32_t) * 2 + paquete -> buffer -> size );
+	offset = 0;
+
+	memcpy(a_enviar+offset, &(paquete->codOp), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+	memcpy(a_enviar+offset, &(paquete->buffer->size), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+	memcpy(a_enviar+offset, paquete->buffer->stream, paquete->buffer->size);
+
+
+
+	send(socket_cliente, a_enviar, sizeof(uint32_t) * 2 + paquete -> buffer -> size, 0);
+
+	free(infoProceso.listaInstrucciones);
+	close(socket_cliente);
+	config_destroy(config);
 	free(archivo);
 	return EXIT_SUCCESS;
 }
 
 
+t_config* inicializarConfigs(void) {
+			t_config* nuevo_config;
+
+			nuevo_config = config_create("/home/utnso/tp/tp/consola/consola.config");
+
+			return nuevo_config; //Creo que esto funciona así igual pero no estoy seguro
+}
 
 
+int crear_conexion(char *ip, char* puerto)
+{
+	struct addrinfo hints;
+	struct addrinfo *server_info;
 
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
+	getaddrinfo(ip, puerto, &hints, &server_info);
+
+	// Ahora vamos a crear el socket. LISTO
+	int socket_cliente = socket( server_info -> ai_family,
+								 server_info -> ai_socktype,
+								 server_info -> ai_protocol);
+
+	// Ahora que tenemos el socket, vamos a conectarlo LISTO
+
+	connect(socket_cliente, server_info -> ai_addr, server_info -> ai_addrlen );
+
+	freeaddrinfo(server_info);
+
+	return socket_cliente;
+}
 
 
 
--- consola/consola.h
+++ consola/consola.h
@@ -1,8 +1,34 @@
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/string.h>
 #include <commons/config.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <string.h>
+#include <unistd.h>
+#define CREAR_PROCESO 1
 
 void pushearInstruccion(char* instruccion, char*** listaInstrucciones);
+t_config* inicializarConfigs(void);
+int crear_conexion(char* ip_kernel, char* puerto_kernel);
+
+typedef struct {
+	uint32_t tamanioDirecciones;
+	uint32_t largoListaInstrucciones;
+	char* listaInstrucciones;
+} t_info_proceso;
+
+typedef struct {
+	uint32_t size;
+	void* stream;
+} t_buffer;
+
+typedef struct {
+	uint32_t codOp;
+	t_buffer* buffer;
+} t_paquete;
+
+#endif
