c7576026864bd5962c08c65ec0f62d8c40540a81
gdepablo <guille_de_pablo@outlook.com>
2022-06-10 02:18:04 -0300

    WIP Memoria

--- memoria/memoria.h
+++ memoria/memoria.h
@@ -7,6 +7,9 @@
 #include <sys/socket.h>
 #include <commons/string.h>
 #include <commons/config.h>
+#include <commons/collections/queue.h>
+#include <commons/collections/list.h>
+#include <stdbool.h>
 
 
 
@@ -18,3 +21,31 @@ typedef struct{
 	uint32_t tamanio_paginas;
 	uint32_t entradas_por_tabla;
 } info_traduccion_t;
+
+
+typedef struct {
+	uint32_t frame;
+	uint32_t tamanio_frame;
+	paginas_primer_nivel* paginas;
+} tabla_paginas_t;
+
+typedef struct {
+	uint32_t frame;
+	uint32_t tamanio_frame;
+	paginas_segundo_nivel* paginas;
+	char* algoritmo_swapeo_paginas;
+} tabla_paginas_segundo_nivel_t;
+
+typedef struct {
+	uint32_t numero_frame;
+	uint32_t pnumero_TP_segundo_nivel;
+} paginas_primer_nivel;
+
+
+typedef struct {
+	uint32_t numero_pagina;
+	uint32_t data_pagina;
+	bool bit_presencia;
+	bool bit_uso;
+	bool bit_modificacion; // P U M
+} paginas_segundo_nivel;
